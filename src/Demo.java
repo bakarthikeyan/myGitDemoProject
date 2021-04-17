27ae70aa738415344ee44b8846879b331f7957cd
KarthikeyanB <karthikeyan.balasubramanian@wipro.com>
2021-04-17 15:06:57 +0530

    This is my first commit

---
+++ src/Demo.java
@@ -0,0 +1,7 @@
+
+public class Demo{
+	public static void main(String[] args) {
+		String check_demo = "Demo code";
+		System.out.println(check_demo);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module myGitDemoProject {
+}
